8930574f07ce8206a3c42faa422aa3a0340962f8
Ashu4999 <ashutoshgorke77@gmail.com>
2024-09-15 18:00:08 +0530

    user routes structure changed

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -93,5 +93,146 @@ namespace Learning_Dotnet.Controllers
             }
             throw new Exception("Failed to delete user");
         }
+
+        [HttpGet("UserJobInfo")]
+        public IEnumerable<UserJobInfo> GetUserJobInfo([FromQuery] int pageNo, [FromQuery] int pageSize)
+        {
+            string sql = "SELECT * FROM TutorialAppSchema.UserJobInfo";
+
+            if (pageNo > 0 && pageSize > 0)
+            {
+                int skip = (pageNo - 1) * pageSize;
+                sql += $" ORDER BY UserId OFFSET {skip} ROWS FETCH NEXT {pageSize} ROWS ONLY;";
+            }
+            IEnumerable<UserJobInfo> userJobInfos = _dataContextDapper.LoadData<UserJobInfo>(sql);
+            return userJobInfos;
+        }
+
+        [HttpGet("UserJobInfo/{userId}")]
+        public IActionResult GetUserJobInfo([FromRoute] int userId)
+        {
+            string sql = $"SELECT * FROM TutorialAppSchema.UserJobInfo WHERE UserId = {userId}";
+            UserJobInfo foundUserJobInfo = _dataContextDapper.LoadDataSingle<UserJobInfo>(sql);
+            return Ok(foundUserJobInfo);
+        }
+
+        [HttpPost("UserJobInfo")]
+        public IActionResult AddUserJobInfo(UserJobInfo userJobInfo)
+        {
+            string sql = @$"
+            INSERT INTO TutorialAppSchema.UserJobInfo(
+	            [UserId], [JobTitle], [Department]
+            ) VALUES
+                ('{userJobInfo.UserId}', '{userJobInfo.JobTitle}', '{userJobInfo.Department}'
+            );";
+            bool result = _dataContextDapper.ExecuteSql(sql);
+            if (result)
+            {
+                return Ok();
+            }
+            throw new Exception("Failed to add userJobInfo");
+        }
+
+        [HttpPut("UserJobInfo")]
+        public IActionResult EditUserJobInfo(UserJobInfo userJobInfo)
+        {
+            string sql = @$"
+            UPDATE TutorialAppSchema.UserJobInfo
+                SET
+                    UserId = {userJobInfo.UserId},
+                    JobTitle = '{userJobInfo.JobTitle}', 
+                    Department = '{userJobInfo.Department}'
+                WHERE 
+                    UserId = {userJobInfo.UserId};
+            ";
+            bool result = _dataContextDapper.ExecuteSql(sql);
+            if (result)
+            {
+                return Ok();
+            }
+            throw new Exception("Failed to edit userJobInfo");
+        }
+
+        [HttpDelete("UserJobInfo/{userId}")]
+        public IActionResult DeleteUserJobInfo([FromRoute] int userId)
+        {
+            string sql = $"DELETE FROM TutorialAppSchema.UserJobInfo WHERE UserId = {userId}";
+            bool result = _dataContextDapper.ExecuteSql(sql);
+            if (result)
+            {
+                return Ok();
+            }
+            throw new Exception("Failed to delete userJobInfo");
+        }
+
+        [HttpGet("UserSalary")]
+        public IEnumerable<UserSalary> GetUserSalary([FromQuery] int pageNo, [FromQuery] int pageSize)
+        {
+            string sql = "SELECT * FROM TutorialAppSchema.UserSalary";
+
+            if (pageNo > 0 && pageSize > 0)
+            {
+                int skip = (pageNo - 1) * pageSize;
+                sql += $" ORDER BY UserId OFFSET {skip} ROWS FETCH NEXT {pageSize} ROWS ONLY;";
+            }
+            IEnumerable<UserSalary> userSalaries = _dataContextDapper.LoadData<UserSalary>(sql);
+            return userSalaries;
+        }
+
+        [HttpGet("UserSalary/{userId}")]
+        public IActionResult GetUserSalary([FromRoute] int userId)
+        {
+            string sql = $"SELECT * FROM TutorialAppSchema.UserSalary WHERE UserId = {userId}";
+            UserJobInfo foundUserSalary = _dataContextDapper.LoadDataSingle<UserJobInfo>(sql);
+            return Ok(foundUserSalary);
+        }
+
+        [HttpPost("UserSalary")]
+        public IActionResult AddUserSalary(UserSalary userSalary)
+        {
+            string sql = @$"
+            INSERT INTO TutorialAppSchema.UserSalary(
+	            [UserId], [Salary]
+            ) VALUES
+                ('{userSalary.UserId}', '{userSalary.Salary}'
+            );";
+            bool result = _dataContextDapper.ExecuteSql(sql);
+            if (result)
+            {
+                return Ok();
+            }
+            throw new Exception("Failed to add userSalary");
+        }
+
+        [HttpPut("UserSalary")]
+        public IActionResult EditUserSalary(UserSalary userSalary)
+        {
+            string sql = @$"
+            UPDATE TutorialAppSchema.UserSalary
+                SET
+                    UserId = {userSalary.UserId},
+                    Salary = '{userSalary.Salary}'
+                WHERE 
+                    UserId = {userSalary.UserId};
+            ";
+            bool result = _dataContextDapper.ExecuteSql(sql);
+            if (result)
+            {
+                return Ok();
+            }
+            throw new Exception("Failed to edit userSalary");
+        }
+
+        [HttpDelete("UserSalary/{userId}")]
+        public IActionResult DeleteUserSalary([FromRoute] int userId)
+        {
+            string sql = $"DELETE FROM TutorialAppSchema.UserSalary WHERE UserId = {userId}";
+            bool result = _dataContextDapper.ExecuteSql(sql);
+            if (result)
+            {
+                return Ok();
+            }
+            throw new Exception("Failed to delete userSalary");
+        }
     }
 }
--- Controllers/UserEFController.cs
+++ Controllers/UserEFController.cs
@@ -92,5 +92,126 @@ namespace Learning_Dotnet.Controllers
             _dataContextEF.SaveChanges();
             return NoContent();
         }
+
+        [HttpGet("UserJobInfo")]
+        public IEnumerable<UserJobInfo> GetUserJobInfo([FromQuery] int pageNo, [FromQuery] int pageSize)
+        {
+            IQueryable<UserJobInfo> userJobInfoQuery = _dataContextEF.UserJobInfo;
+
+            if (pageNo > 0 && pageSize > 0)
+            {
+                int skip = (pageNo - 1) * pageSize;
+                userJobInfoQuery = userJobInfoQuery.Skip(skip).Take(pageSize);
+            }
+            IEnumerable<UserJobInfo> userJobInfos = userJobInfoQuery.ToList();
+            return userJobInfos;
+        }
+
+        [HttpGet("UserJobInfo/{userId}")]
+        public IActionResult GetUserJobInfo([FromRoute] int userId)
+        {
+            UserJobInfo? foundUserJobInfo = _dataContextEF.UserJobInfo.Where(u => u.UserId == userId).FirstOrDefault();
+
+            if (foundUserJobInfo == null)
+                return NotFound();
+
+            return Ok(foundUserJobInfo);
+        }
+
+        [HttpPost("UserJobInfo")]
+        public IActionResult AddUserJobInfo(UserJobInfo userJobInfo)
+        {
+            _dataContextEF.UserJobInfo.Add(userJobInfo);
+            _dataContextEF.SaveChanges();
+            return Ok(userJobInfo);
+        }
+
+        [HttpPut("UserJobInfo")]
+        public IActionResult EditUserJobInfo(UserJobInfo userJobInfo)
+        {
+            UserJobInfo? foundUserJobInfo = _dataContextEF.UserJobInfo.Where(u => u.UserId == userJobInfo.UserId).FirstOrDefault();
+
+            if (foundUserJobInfo == null)
+                return NotFound();
+
+            foundUserJobInfo.JobTitle = userJobInfo.JobTitle;
+            foundUserJobInfo.Department = userJobInfo.Department;
+            _dataContextEF.SaveChanges();
+            return Ok(foundUserJobInfo);
+        }
+
+
+        [HttpDelete("UserJobInfo/{userId}")]
+        public IActionResult DeleteUserJobInfo([FromRoute] int userId)
+        {
+            UserJobInfo? foundUserJobInfo = _dataContextEF.UserJobInfo.Where(u => u.UserId == userId).FirstOrDefault();
+
+            if (foundUserJobInfo == null)
+                return NotFound();
+
+            _dataContextEF.UserJobInfo.Remove(foundUserJobInfo);
+            _dataContextEF.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpGet("UserSalary")]
+        public IEnumerable<UserSalary> GetUserSalaries([FromQuery] int pageNo, [FromQuery] int pageSize)
+        {
+            IQueryable<UserSalary> userSalaryQuery = _dataContextEF.UserSalary;
+
+            if (pageNo > 0 && pageSize > 0)
+            {
+                int skip = (pageNo - 1) * pageSize;
+                userSalaryQuery = userSalaryQuery.Skip(skip).Take(pageSize);
+            }
+            IEnumerable<UserSalary> userSalaries = userSalaryQuery.ToList();
+            return userSalaries;
+        }
+
+        [HttpGet("UserSalary/{userId}")]
+        public IActionResult GetUserSalary([FromRoute] int userId)
+        {
+            UserSalary? foundUserSalary = _dataContextEF.UserSalary.Where(u => u.UserId == userId).FirstOrDefault();
+
+            if (foundUserSalary == null)
+                return NotFound();
+
+            return Ok(foundUserSalary);
+        }
+
+        [HttpPost("UserSalary")]
+        public IActionResult AddUserSalary(UserSalary userSalary)
+        {
+            _dataContextEF.UserSalary.Add(userSalary);
+            _dataContextEF.SaveChanges();
+            return Ok(userSalary);
+        }
+
+        [HttpPut("UserSalary")]
+        public IActionResult EditUserSalary(UserSalary userSalary)
+        {
+            UserSalary? foundUserSalary = _dataContextEF.UserSalary.Where(u => u.UserId == userSalary.UserId).FirstOrDefault();
+
+            if (foundUserSalary == null)
+                return NotFound();
+
+            foundUserSalary.Salary = userSalary.Salary;
+
+            _dataContextEF.SaveChanges();
+            return Ok(foundUserSalary);
+        }
+
+        [HttpDelete("UserSalary/{userId}")]
+        public IActionResult DeleteUserSalary([FromRoute] int userId)
+        {
+            UserSalary? foundUserSalary = _dataContextEF.UserSalary.Where(u => u.UserId == userId).FirstOrDefault();
+
+            if (foundUserSalary == null)
+                return NotFound();
+
+            _dataContextEF.UserSalary.Remove(foundUserSalary);
+            _dataContextEF.SaveChanges();
+            return NoContent();
+        }
     }
 }
